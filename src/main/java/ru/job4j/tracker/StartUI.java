db9e9401b5c100b3f9f0f8d5dca1c95696e105e7
Evgeny Novoselov <enovoselov@yahoo.com>
2023-07-31 18:49:51 +0300

    Поправил StartUI

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,18 +6,19 @@ import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.ValidateInput;
 import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
-import ru.job4j.tracker.store.MemTracker;
 import ru.job4j.tracker.store.SqlTracker;
 import ru.job4j.tracker.store.Store;
 
 import java.util.List;
 
 public class StartUI {
+    private static int actionsCount;
+
     public void init(Input input, Store tracker, List<UserAction> actions) {
         boolean run = true;
         while (run) {
             showMenu(actions);
-            int select = input.askInt("Enter select: ");
+            int select = input.askInt("Enter select: ", actionsCount);
             UserAction action = actions.get(select);
             run = action.execute(input, tracker);
         }
@@ -46,6 +47,7 @@ public class StartUI {
                     new FindByNameAction(output),
                     new ExitAction()
             );
+            actionsCount = actions.size();
             new StartUI().init(validate, tracker, actions);
         } catch (Exception e) {
             e.printStackTrace();
